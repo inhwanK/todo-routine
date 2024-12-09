20d5ad7d6325736183930a7d953a784535503eec
inhwan <inhan1009@naver.com>
2024-12-10 01:14:33 +0900

    initial commit

---
+++ src/main/java/ind/venture/todoroutine/TodoRoutineApplication.java
@@ -0,0 +1,13 @@
+package ind.venture.todoroutine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoRoutineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodoRoutineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ind/venture/todoroutine/TodoRoutineApplicationTests.java
@@ -0,0 +1,13 @@
+package ind.venture.todoroutine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoRoutineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
